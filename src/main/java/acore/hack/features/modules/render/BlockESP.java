1edd86464b795a01910ab472bceeb5689bb2baa1
snowmossd-droid <snowmossd@gmail.com>
2026-05-15 04:08:15 +0700

    Create BlockESP.java

---
+++ src/main/java/acore/hack/features/modules/render/BlockESP.java
@@ -0,0 +1,20 @@
+package acore.hack.features.modules.render;
+
+import acore.hack.features.modules.Module;
+
+public class BlockESP extends Module {
+    
+    public BlockESP() {
+        super("BlockESP", Category.RENDER);
+    }
+    
+    @Override
+    protected void onEnable() {
+        super.onEnable();
+    }
+    
+    @Override
+    protected void onDisable() {
+        super.onDisable();
+    }
+}
