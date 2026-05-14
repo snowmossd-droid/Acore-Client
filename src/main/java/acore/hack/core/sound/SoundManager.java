431bee2c99032f9d29afee31c31240fcd8ebae66
snowmossd-droid <snowmossd@gmail.com>
2026-05-15 04:07:29 +0700

    Create SoundManager.java

---
+++ src/main/java/acore/hack/core/sound/SoundManager.java
@@ -0,0 +1,15 @@
+package acore.hack.core.sound;
+
+public class SoundManager {
+    public static void init() {
+        // Initialize sounds
+    }
+    
+    public static void playClickSound() {
+        // Play click sound
+    }
+    
+    public static void playToggleSound() {
+        // Play toggle sound
+    }
+}
