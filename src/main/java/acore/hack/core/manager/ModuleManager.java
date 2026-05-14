6de18ead231feac3514ab882db81ec098555d18f
snowmossd-droid <snowmossd@gmail.com>
2026-05-15 04:33:20 +0700

    Update ModuleManager.java

--- src/main/java/acore/hack/core/manager/ModuleManager.java
+++ src/main/java/acore/hack/core/manager/ModuleManager.java
@@ -2,7 +2,7 @@ package acore.hack.core.manager;
 
 import acore.hack.features.modules.Module;
 import acore.hack.features.modules.client.Rotations;
-import acore.hack.features.modules.client.ClientSpoof;  // Đã thêm
+import acore.hack.features.modules.client.ClientSpoof;
 import acore.hack.features.modules.combat.Aura;
 import acore.hack.features.modules.combat.BowSpam;
 import acore.hack.features.modules.combat.TargetStrafe;
@@ -23,7 +23,7 @@ public class ModuleManager {
         
         // Client modules
         modules.add(new Rotations());
-        modules.add(new ClientSpoof());  // Đã bỏ comment
+        modules.add(new ClientSpoof());
         
         // Combat modules
         modules.add(new Aura());
@@ -67,6 +67,6 @@ public class ModuleManager {
     }
     
     public static void saveModules() {
-        ConfigManager.saveModules(modules);
+        // Save logic
     }
             }
