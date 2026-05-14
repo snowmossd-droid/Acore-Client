a1dd2994c6b8d727cc929e5ef916d7c261fcfd3f
snowmossd-droid <snowmossd@gmail.com>
2026-05-15 04:09:47 +0700

    Create AutoTNTcart.java

---
+++ src/main/java/acore/hack/features/modules/combat/AutoTNTcart.java
@@ -0,0 +1,9 @@
+package acore.hack.features.modules.combat;
+
+import acore.hack.features.modules.Module;
+
+public class AutoTNTcart extends Module {
+    public AutoTNTcart() {
+        super("AutoTNTcart", Category.COMBAT);
+    }
+}
