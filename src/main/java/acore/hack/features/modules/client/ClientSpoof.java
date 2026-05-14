3d3d5c91fc99320ab566dbf2611a1eff428fafdd
snowmossd-droid <snowmossd@gmail.com>
2026-05-15 04:32:48 +0700

    Update ClientSpoof.java

--- src/main/java/acore/hack/features/modules/client/ClientSpoof.java
+++ src/main/java/acore/hack/features/modules/client/ClientSpoof.java
@@ -1,38 +1,20 @@
 package acore.hack.features.modules.client;
 
 import acore.hack.features.modules.Module;
-import net.minecraft.network.packet.c2s.common.ServerBrandC2SPacket;
 
 public class ClientSpoof extends Module {
+
+    public ClientSpoof() {
+        super("ClientSpoof", Category.CLIENT);
+    }
+
+    public Mode mode = Mode.Vanilla;
+    public String custom = "feather";
 
     public enum Mode {
         Vanilla, Lunar1_20_4, Lunar1_20_1, Custom, Null
     }
-    
-    public Mode mode = Mode.Vanilla;
-    public String customClient = "feather";
-    
-    public ClientSpoof() {
-        super("ClientSpoof", Category.CLIENT);
-    }
-    
-    @Override
-    protected void onEnable() {
-        sendBrand();
-    }
-    
-    @Override
-    public void onUpdate() {
-        sendBrand();
-    }
-    
-    private void sendBrand() {
-        String brand = getClientName();
-        if (brand != null && mc.getNetworkHandler() != null) {
-            mc.getNetworkHandler().sendPacket(new ServerBrandC2SPacket(brand));
-        }
-    }
-    
+
     public String getClientName() {
         switch (mode) {
             case Vanilla:
@@ -42,11 +24,9 @@ public class ClientSpoof extends Module {
             case Lunar1_20_1:
                 return "lunarclient:1.20.1";
             case Custom:
-                return customClient;
-            case Null:
-                return null;
+                return custom;
             default:
-                return "vanilla";
+                return null;
         }
     }
 }
