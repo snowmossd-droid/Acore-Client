8c042400a15f601da9ab58ddd25793efe39af6b4
snowmossd-droid <snowmossd@gmail.com>
2026-05-15 04:08:59 +0700

    Create BowSpam.java

---
+++ src/main/java/acore/hack/features/modules/combat/BowSpam.java
@@ -0,0 +1,9 @@
+package acore.hack.features.modules.combat;
+
+import acore.hack.features.modules.Module;
+
+public class BowSpam extends Module {
+    public BowSpam() {
+        super("BowSpam", Category.COMBAT);
+    }
+}
