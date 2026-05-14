fcfbef849c845e9a9b700c3b2e02970f543d9cc2
snowmossd-droid <snowmossd@gmail.com>
2026-05-15 04:09:28 +0700

    Create HitBox.java

---
+++ src/main/java/acore/hack/features/modules/combat/HitBox.java
@@ -0,0 +1,9 @@
+package acore.hack.features.modules.combat;
+
+import acore.hack.features.modules.Module;
+
+public class HitBox extends Module {
+    public HitBox() {
+        super("HitBox", Category.COMBAT);
+    }
+}
