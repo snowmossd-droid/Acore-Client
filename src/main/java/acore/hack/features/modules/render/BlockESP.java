fe57e33a322053f954de6883c91394baec73ce86
snowmossd-droid <snowmossd@gmail.com>
2026-05-15 14:13:02 +0700

    Update BlockESP.java

--- src/main/java/acore/hack/features/modules/render/BlockESP.java
+++ src/main/java/acore/hack/features/modules/render/BlockESP.java
@@ -2,18 +2,12 @@ package acore.hack.features.modules.render;
 
 import acore.hack.features.modules.Module;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.client.render.*;
-import net.minecraft.client.util.math.MatrixStack;
-import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BlockESP extends Module {
     
     private List<BlockPos> trackedBlocks = new ArrayList<>();
-    public Color blockColor = new Color(0xFF0000, true);
     
     public BlockESP() {
         super("BlockESP", Category.RENDER);
@@ -38,61 +32,7 @@ public class BlockESP extends Module {
     }
     
     @Override
-    public void onRender3D() {
-        if (mc.world == null) return;
-        
-        Vec3d camera = mc.gameRenderer.getCamera().getPos();
-        MatrixStack stack = new MatrixStack();
-        
-        for (BlockPos pos : trackedBlocks) {
-            drawBlockESP(stack, pos, camera);
-        }
+    public void onRender() {
+        // Block ESP rendering will be implemented later
     }
-    
-    private void drawBlockESP(MatrixStack stack, BlockPos pos, Vec3d camera) {
-        double x = pos.getX() - camera.x;
-        double y = pos.getY() - camera.y;
-        double z = pos.getZ() - camera.z;
-        
-        stack.push();
-        stack.translate(x, y, z);
-        
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder buffer = tessellator.begin(VertexFormat.DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR);
-        
-        // Draw box outline
-        buffer.vertex(0, 0, 0).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(1, 0, 0).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(1, 0, 0).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(1, 0, 1).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(1, 0, 1).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(0, 0, 1).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(0, 0, 1).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(0, 0, 0).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        
-        buffer.vertex(0, 1, 0).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(1, 1, 0).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(1, 1, 0).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(1, 1, 1).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(1, 1, 1).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(0, 1, 1).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(0, 1, 1).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(0, 1, 0).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        
-        buffer.vertex(0, 0, 0).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(0, 1, 0).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(1, 0, 0).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(1, 1, 0).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(1, 0, 1).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(1, 1, 1).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(0, 0, 1).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        buffer.vertex(0, 1, 1).color(blockColor.getRed(), blockColor.getGreen(), blockColor.getBlue(), 255);
-        
-        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
-        BufferBuilder.BuildResult result = buffer.end();
-        BufferDrawer drawer = tessellator.draw(result);
-        drawer.draw();
-        
-        stack.pop();
-    }
-            }
+}
