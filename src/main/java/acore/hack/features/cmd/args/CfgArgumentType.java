ab86c405210f0494419deb1a9ac51430e816480a
snowmossd-droid <snowmossd@gmail.com>
2026-05-20 06:24:44 +0700

    Create CfgArgumentType.java

---
+++ src/main/java/acore/hack/features/cmd/args/CfgArgumentType.java
@@ -0,0 +1,39 @@
+package acore.hack.features.cmd.args;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+import net.minecraft.command.CommandSource;
+import net.minecraft.text.Text;
+import acore.hack.core.Managers;
+
+public class CfgArgumentType implements ArgumentType<String> {
+   private static final Collection<String> EXAMPLES = Managers.CONFIG.getConfigList().stream().limit(5L).toList();
+
+   public static CfgArgumentType create() {
+      return new CfgArgumentType();
+   }
+
+   public String parse(StringReader reader) throws CommandSyntaxException {
+      String config = reader.readString();
+      if (!Managers.CONFIG.getConfigList().contains(config)) {
+         throw new DynamicCommandExceptionType(name -> Text.literal("Config " + name + " does not exist :(")).create(config);
+      } else {
+         return config;
+      }
+   }
+
+   public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+      return CommandSource.suggestMatching(Managers.CONFIG.getConfigList(), builder);
+   }
+
+   public Collection<String> getExamples() {
+      return EXAMPLES;
+   }
+}
