c72788d8c0425dbac8e2883a781fb9e16ed5112e
snowmossd-droid <snowmossd@gmail.com>
2026-05-20 05:40:36 +0700

    Create EventCollision.java

---
+++ src/main/java/acore/hack/event/impl/EventCollision.java
@@ -0,0 +1,27 @@
+package acore.hack.event.impl;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import acore.hack.event.Event;
+
+public class EventCollision extends Event {
+   private BlockState bs;
+   private BlockPos bp;
+
+   public EventCollision(BlockState bs, BlockPos bp) {
+      this.bs = bs;
+      this.bp = bp;
+   }
+
+   public BlockState getState() {
+      return this.bs;
+   }
+
+   public BlockPos getPos() {
+      return this.bp;
+   }
+
+   public void setState(BlockState bs) {
+      this.bs = bs;
+   }
+}
