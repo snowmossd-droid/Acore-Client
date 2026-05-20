039bc8909ed93610125236011be97951b40b17a5
snowmossd-droid <snowmossd@gmail.com>
2026-05-20 21:13:08 +0700

    Create IClientPlayerEntity.java

---
+++ src/main/java/acore/hack/injection/accesors/IClientPlayerEntity.java
@@ -0,0 +1,27 @@
+package acore.hack.injection.accesors;
+
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(ClientPlayerEntity.class)
+public interface IClientPlayerEntity {
+   @Invoker("sendMovementPackets")
+   void iSendMovementPackets();
+
+   @Accessor("lastYaw")
+   float getLastYaw();
+
+   @Accessor("lastPitch")
+   float getLastPitch();
+
+   @Accessor("lastYaw")
+   void setLastYaw(float var1);
+
+   @Accessor("lastPitch")
+   void setLastPitch(float var1);
+
+   @Accessor("mountJumpStrength")
+   void setMountJumpStrength(float var1);
+}
