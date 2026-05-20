d2cc7f90a54b2eee9f5cdf2183fa36cae0a43857
snowmossd-droid <snowmossd@gmail.com>
2026-05-21 03:47:26 +0700

    Create Texture.java

---
+++ src/main/java/acore/hack/gui/font/Texture.java
@@ -0,0 +1,35 @@
+package acore.hack.gui.font;
+
+import net.minecraft.util.Identifier;
+
+public class Texture {
+   final Identifier id;
+
+   public Texture(String path) {
+      this.id = Identifier.of("acore", this.validatePath(path));
+   }
+
+   public Texture(Identifier i) {
+      this.id = Identifier.of(i.getNamespace(), i.getPath());
+   }
+
+   String validatePath(String path) {
+      if (Identifier.isPathValid(path)) {
+         return path;
+      }
+
+      StringBuilder ret = new StringBuilder();
+
+      for (char c : path.toLowerCase().toCharArray()) {
+         if (Identifier.isPathCharacterValid(c)) {
+            ret.append(c);
+         }
+      }
+
+      return ret.toString();
+   }
+
+   public Identifier getId() {
+      return this.id;
+   }
+}
