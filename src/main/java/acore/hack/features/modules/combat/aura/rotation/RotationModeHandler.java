7fd0e8907a781d6ee9ba7508bc15f2a4efb3b782
snowmossd-droid <snowmossd@gmail.com>
2026-05-21 02:28:42 +0700

    Create RotationModeHandler.java

---
+++ src/main/java/acore/hack/features/modules/combat/aura/rotation/RotationModeHandler.java
@@ -0,0 +1,7 @@
+package acore.hack.features.modules.combat.aura.rotation;
+
+import acore.hack.features.modules.combat.Aura;
+
+public interface RotationModeHandler {
+   void rotate(Aura var1, boolean var2);
+}
