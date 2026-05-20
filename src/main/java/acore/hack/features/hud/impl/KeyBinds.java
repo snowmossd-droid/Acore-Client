e94d76e0562875ae793eae19c5199651d92ff481
snowmossd-droid <snowmossd@gmail.com>
2026-05-20 20:46:59 +0700

    Update KeyBinds.java

--- src/main/java/acore/hack/features/hud/impl/KeyBinds.java
+++ src/main/java/acore/hack/features/hud/impl/KeyBinds.java
@@ -7,7 +7,7 @@ import java.util.function.Supplier;
 import net.minecraft.client.gui.DrawContext;
 import org.jetbrains.annotations.NotNull;
 import acore.hack.core.Managers;
-import acore.hack.core.manager.client.ModuleManager;
+import acore.hack.core.manager.ModuleManager;
 import acore.hack.features.hud.HudElement;
 import acore.hack.features.modules.Module;
 import acore.hack.features.modules.render.HudEditor;
