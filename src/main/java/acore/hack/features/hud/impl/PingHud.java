41474158cfb394fb87626a5675dd601473d9cf3e
snowmossd-droid <snowmossd@gmail.com>
2026-05-20 18:38:40 +0700

    Create PingHud.java

---
+++ src/main/java/acore/hack/features/hud/impl/PingHud.java
@@ -0,0 +1,56 @@
+package acore.hack.features.hud.impl;
+
+import com.mojang.blaze3d.platform.GlStateManager.DstFactor;
+import com.mojang.blaze3d.platform.GlStateManager.SrcFactor;
+import com.mojang.blaze3d.systems.RenderSystem;
+import java.awt.Color;
+import java.util.List;
+import java.util.function.Supplier;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.util.Formatting;
+import acore.hack.core.Managers;
+import acore.hack.features.hud.HudElement;
+import acore.hack.features.modules.render.HudEditor;
+import acore.hack.gui.font.FontRenderers;
+import acore.hack.setting.impl.PositionSetting;
+import acore.hack.utility.render.Render2DEngine;
+import acore.hack.utility.render.TextureStorage;
+
+public class PingHud extends HudElement {
+   public PingHud(PositionSetting position, Supplier<List<HudElement>> activeElementsSupplier) {
+      super("Ping", 50, 10, position, activeElementsSupplier);
+   }
+
+   @Override
+   public void render(DrawContext context) {
+      super.render(context);
+      String str = "Ping " + Formatting.WHITE + Managers.SERVER.getPing();
+      float width = FontRenderers.getModulesRenderer().getStringWidth(str) + 21.0F;
+      float pX = this.getPosX() > mc.getWindow().getScaledWidth() / 2.0F ? this.getPosX() - width : this.getPosX();
+      Render2DEngine.drawHudBase(context.getMatrices(), pX, this.getPosY(), width, 13.0F, 3.0F);
+      Render2DEngine.drawRect(context.getMatrices(), pX + 14.0F, this.getPosY() + 2.0F, 0.5F, 8.0F, new Color(1157627903, true));
+      Render2DEngine.setupRender();
+      RenderSystem.blendFunc(SrcFactor.SRC_ALPHA, DstFactor.ONE);
+      RenderSystem.setShaderTexture(0, TextureStorage.pingIcon);
+      Render2DEngine.renderGradientTexture(
+         context.getMatrices(),
+         pX + 2.0F,
+         this.getPosY() + 1.0F,
+         10.0,
+         10.0,
+         0.0F,
+         0.0F,
+         512.0,
+         512.0,
+         512.0,
+         512.0,
+         HudEditor.getColor(270),
+         HudEditor.getColor(0),
+         HudEditor.getColor(180),
+         HudEditor.getColor(90)
+      );
+      Render2DEngine.endRender();
+      FontRenderers.getModulesRenderer().drawString(context.getMatrices(), str, pX + 18.0F, this.getPosY() + 5.0F, HudEditor.getColor(1).getRGB());
+      this.setBounds(pX, this.getPosY(), width, 13.0F);
+   }
+        }
