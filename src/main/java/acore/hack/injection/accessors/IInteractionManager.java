3ee268e284ae14433eea7e339be228f2c4bc5cae
snowmossd-droid <snowmossd@gmail.com>
2026-05-20 07:54:44 +0700

    Create IInteractionManager.java

---
+++ src/main/java/acore/hack/injection/accessors/IInteractionManager.java
@@ -0,0 +1,18 @@
+package acore.hack.injection.accessors;
+
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public interface IInteractionManager {
+   @Accessor("currentBreakingProgress")
+   float getCurBlockDamageMP();
+
+   @Accessor("currentBreakingProgress")
+   void setCurBlockDamageMP(float var1);
+
+   @Invoker("syncSelectedSlot")
+   void syncSlot();
+}
