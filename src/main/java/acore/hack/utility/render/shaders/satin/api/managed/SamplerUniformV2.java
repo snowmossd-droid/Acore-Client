fd561514d5cef3102762d0eda00139bad3ac0eb2
snowmossd-droid <snowmossd@gmail.com>
2026-05-21 00:11:32 +0700

    Create SamplerUniformV2.java

---
+++ src/main/java/acore/hack/utility/render/shaders/satin/api/managed/SamplerUniformV2.java
@@ -0,0 +1,7 @@
+package acore.hack.utility.render.shaders.satin.api.managed.uniform;
+
+import java.util.function.IntSupplier;
+
+public interface SamplerUniformV2 extends SamplerUniform {
+   void set(IntSupplier var1);
+}
