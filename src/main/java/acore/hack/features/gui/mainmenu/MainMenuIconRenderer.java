7e5bfe8514517b45cc79950286053eb577700d4a
snowmossd-droid <snowmossd@gmail.com>
2026-05-21 09:24:50 +0700

    Update MainMenuIconRenderer.java

--- src/main/java/acore/hack/features/gui/mainmenu/MainMenuIconRenderer.java
+++ src/main/java/acore/hack/features/gui/mainmenu/MainMenuIconRenderer.java
@@ -1,15 +1,18 @@
 package acore.hack.features.gui.mainmenu;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import java.awt.Color;
 
 public final class MainMenuIconRenderer {
+    private static final MinecraftClient mc = MinecraftClient.getInstance();
+    
     private MainMenuIconRenderer() {}
     
     public static void render(DrawContext context, MainMenuButton.IconType iconType, float centerX, float centerY, float size, boolean hovered) {
         Color color = hovered ? Color.CYAN : Color.WHITE;
         String icon = getIconChar(iconType);
-        context.drawCenteredTextWithShadow(textRenderer, icon, (int)centerX, (int)(centerY - size / 2f), color.getRGB());
+        context.drawCenteredTextWithShadow(mc.textRenderer, icon, (int)centerX, (int)(centerY - size / 2f), color.getRGB());
     }
     
     private static String getIconChar(MainMenuButton.IconType type) {
@@ -21,4 +24,4 @@ public final class MainMenuIconRenderer {
             case LEAVE -> "🚪";
         };
     }
-    }
+        }
