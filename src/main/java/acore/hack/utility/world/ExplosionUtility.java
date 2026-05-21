690a1e68e04bae45e641a0deafd6853fd3b1ffa7
snowmossd-droid <snowmossd@gmail.com>
2026-05-22 04:34:16 +0700

    Update ExplosionUtility.java

--- src/main/java/acore/hack/utility/world/ExplosionUtility.java
+++ src/main/java/acore/hack/utility/world/ExplosionUtility.java
@@ -5,6 +5,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.DamageUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.attribute.EntityAttributes;
