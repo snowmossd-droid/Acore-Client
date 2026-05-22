35790f93675b94ed5a1308b854d7951b8192132c
snowmossd-droid <snowmossd@gmail.com>
2026-05-23 01:56:16 +0700

    Update EventBus.java

--- src/main/java/acore/hack/event/EventBus.java
+++ src/main/java/acore/hack/event/EventBus.java
@@ -1,25 +1,24 @@
 package acore.hack.event;
 
 import meteordevelopment.orbit.IEventBus;
-// import meteordevelopment.orbit.EventBus;
 import java.lang.invoke.MethodHandles;
 
 public class EventBus {
-    private static final IEventBus EVENT_BUS = new EventBus();
-    
+    private static final IEventBus EVENT_BUS = new meteordevelopment.orbit.EventBus();
+
     static {
-        EVENT_BUS.registerLambdaFactory("acore.hack.event.impl", (lookupInMethod, klass) -> 
+        EVENT_BUS.registerLambdaFactory("acore.hack.event.impl", (lookupInMethod, klass) ->
             (MethodHandles.Lookup) lookupInMethod.invoke(null, klass, MethodHandles.lookup()));
     }
-    
+
     public void subscribe(Object object) {
         EVENT_BUS.subscribe(object);
     }
-    
+
     public void unsubscribe(Object object) {
         EVENT_BUS.unsubscribe(object);
     }
-    
+
     public void post(Object event) {
         EVENT_BUS.post(event);
     }
