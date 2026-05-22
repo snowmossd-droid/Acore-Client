b40d3eb52e693f668c916c6c726c28df60d2820e
snowmossd-droid <snowmossd@gmail.com>
2026-05-23 02:13:50 +0700

    Add files via upload

---
+++ src/main/java/acore/hack/utility/render/VenShaderProgram.java
@@ -0,0 +1,61 @@
+package acore.hack.utility.render;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.ShaderProgram;
+import net.minecraft.client.gl.Uniform;
+import net.minecraft.client.render.VertexFormats;
+
+public class VenShaderProgram {
+   protected final ShaderProgram program;
+
+   public VenShaderProgram(String vertex, String fragment) {
+      this.program = new ShaderProgram(
+         MinecraftClient.getInstance().getResourceManager(),
+         "ariscore:shaders/core/" + vertex,
+         VertexFormats.POSITION_COLOR
+      );
+   }
+
+   public void use() {
+      this.program.bind();
+   }
+
+   public Uniform getUniform(String name) {
+      return this.program.getUniform(name);
+   }
+
+   public void setUniform(String name, float value) {
+      Uniform uniform = this.getUniform(name);
+      if (uniform != null) {
+         uniform.set(value);
+      }
+   }
+
+   public void setUniform(String name, int value) {
+      Uniform uniform = this.getUniform(name);
+      if (uniform != null) {
+         uniform.set(value);
+      }
+   }
+
+   public void setUniform(String name, float v1, float v2) {
+      Uniform uniform = this.getUniform(name);
+      if (uniform != null) {
+         uniform.set(v1, v2);
+      }
+   }
+
+   public void setUniform(String name, float v1, float v2, float v3) {
+      Uniform uniform = this.getUniform(name);
+      if (uniform != null) {
+         uniform.set(v1, v2, v3);
+      }
+   }
+
+   public void setUniform(String name, float v1, float v2, float v3, float v4) {
+      Uniform uniform = this.getUniform(name);
+      if (uniform != null) {
+         uniform.set(v1, v2, v3, v4);
+      }
+   }
+}
